947ebe41d7f6d15345f4e62e92afe7ca3fd59cd1
eugene <eugene@eugenes-MacBook-Pro.local>
2017-05-21 18:15:13 +0300

    felte some files

--- client/clientBullet.js
+++
@@ -1,9 +0,0 @@
-export default class Bullet{
-    constructor(initializationPackage){
-        this.id=initializationPackage.id;
-        this.number=initializationPackage.number;
-        this.x=initializationPackage.x;
-        this.y=initializationPackage.y;
-
-    }
-};
--- client/clientPlayer.js
+++
@@ -1,9 +0,0 @@
-export default class Player{
-constructor(initializationPackage){
-    this.id=initializationPackage.id;
-    this.number=initializationPackage.number;
-    this.x=initializationPackage.x;
-    this.y=initializationPackage.y;
-}
-
-};
